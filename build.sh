19efa60f4d366b6571a959d5c5f136fb0c60d415
苏晓晴 <37541680+Suxiaoqinx@users.noreply.github.com>
2025-05-14 03:57:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -85,7 +85,7 @@ sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./external/dtc/scripts
 
 ./build_with_bazel.py -t pineapple gki
 
-cd /kernel_workspace/kernel_platform/out/msm-kernel-pineapple-gki/host
+cd /kernel_workspace/kernel_platform/out/msm-kernel-pineapple-gki/dist
 curl -LO https://github.com/ShirkNeko/SukiSU_KernelPatch_patch/releases/download/0.11-beta/patch_linux
 chmod +x patch_linux
 ./patch_linux
