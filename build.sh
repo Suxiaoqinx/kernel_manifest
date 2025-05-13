0d32c20c43a58dde9bcf5e91e139a0eed2d0d0c9
苏晓晴 <37541680+Suxiaoqinx@users.noreply.github.com>
2025-05-13 20:25:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,42 +22,82 @@ cd "$CONFIG"
 cd kernel_platform
 rm common/android/abi_gki_protected_exports_* || echo "No protected exports!"
 rm msm-kernel/android/abi_gki_protected_exports_* || echo "No protected exports!"
-sed -i 's|echo "\$res"|echo "-android14-11-o-v$(date +%Y%m%d)"|' common/scripts/setlocalversion
-sed -i 's|echo "\$res"|echo "-android14-11-o-v$(date +%Y%m%d)"|' msm-kernel/scripts/setlocalversion
-sed -i 's/ -dirty//g' external/dtc/scripts/setlocalversion
-sed -i 's/SUBLEVEL = 68/SUBLEVEL = 75/' msm-kernel/Makefile
+sed -i 's/ -dirty//g' ./common/scripts/setlocalversion
+sed -i 's/ -dirty//g' ./msm-kernel/scripts/setlocalversion
+sed -i 's/ -dirty//g' ./external/dtc/scripts/setlocalversion
+sed -i '$i res=$(echo "$res" | sed '\''s/-dirty//g'\'')' ./common/scripts/setlocalversion
+sed -i '$i res=$(echo "$res" | sed '\''s/-dirty//g'\'')' ./msm-kernel/scripts/setlocalversion
+sed -i '$i res=$(echo "$res" | sed '\''s/-dirty//g'\'')' ./external/dtc/scripts/setlocalversion
+sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./common/scripts/setlocalversion   
+sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./msm-kernel/scripts/setlocalversion
+sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./external/dtc/scripts/setlocalversion
 
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-cd KernelSU-Next
-export KSU_VERSION=$(expr $(/usr/bin/git rev-list --count HEAD) "+" 10200)
-export KSUVER=$KSU_VERSION
+curl -LSs "https://raw.githubusercontent.com/ShirkNeko/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-dev
+ cd KernelSU
+ KSU_VERSION=$(expr $(/usr/bin/git rev-list --count main) "+" 10606)
+ echo "KSUVER=$KSU_VERSION" >> GITHUB_ENV
+ export KSU_VERSION=$KSU_VERSION
+ sed -i "s/DKSU_VERSION=12800/DKSU_VERSION=${KSU_VERSION}/" kernel/Makefile
 
 cd ../../
-git clone https://gitlab.com/simonpunk/susfs4ksu.git -b "$SUSFS_BRANCH" --depth=1
-git clone https://github.com/TheWildJames/kernel_patches.git  --depth=1
+git clone https://gitlab.com/simonpunk/susfs4ksu.git -b gki-android14-6.1
+git clone https://github.com/Xiaomichael/kernel_patches.git
+git clone https://github.com/ShirkNeko/SukiSU_patch.git
 
 cd kernel_platform
-cp ../susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch ./KernelSU-Next/
-cp ../susfs4ksu/kernel_patches/50_add_susfs_in_gki-${ANYKERNEL_BRANCH}.patch common/
-cp ../kernel_patches/KernelSU-Next-Implement-SUSFS-${SUSFS_VERSION}-Universal.patch ./KernelSU-Next/
+cp ../susfs4ksu/kernel_patches/50_add_susfs_in_gki-android14-6.1.patch ./common/
+cp ../kernel_patches/next/syscall_hooks.patch ./common/
 cp ../susfs4ksu/kernel_patches/fs/* ./common/fs/
 cp ../susfs4ksu/kernel_patches/include/linux/* ./common/include/linux/
-
+cp -r ../SukiSU_patch/other/zram/lz4k/include/linux/* ./common/include/linux
+cp -r ../SukiSU_patch/other/zram/lz4k/lib/* ./common/lib
+cp -r ../SukiSU_patch/other/zram/lz4k/crypto/* ./common/crypto
 cd KernelSU-Next
 patch -p1 < KernelSU-Next-Implement-SUSFS-${SUSFS_VERSION}-Universal.patch || true
 cd ../common
-patch -p1 < 50_add_susfs_in_gki-${ANYKERNEL_BRANCH}.patch || true
+patch -p1 < 50_add_susfs_in_gki-android14-6.1.patch || true
 cp ../../kernel_patches/69_hide_stuff.patch ./
 patch -p1 -F 3 < 69_hide_stuff.patch
-patch -p1 < ../../.repo/manifests/patches/001-lz4.patch
-patch -p1 < ../../.repo/manifests/patches/002-zstd.patch
+patch -p1 -F 3 < syscall_hooks.patch
+cp ../../SukiSU_patch/other/zram/zram_patch/6.1/lz4kd.patch ./
+patch -p1 -F 3 < lz4kd.patch || true
+
+echo "CONFIG_KSU=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KPM=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_SUS_SU=n" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_MANUAL_HOOK=y" >> ./common/arch/arm64/configs/gki_defconfig      
+echo "CONFIG_KSU_SUSFS=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_SUS_OVERLAYFS=n" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_TRY_UMOUNT=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_ZSMALLOC=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_CRYPTO_LZ4HC=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_CRYPTO_LZ4K=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_CRYPTO_LZ4KD=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_CRYPTO_842=y" >> ./common/arch/arm64/configs/gki_defconfig
+sed -i 's/check_defconfig//' ./common/build.config.gki
+
+sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./common/scripts/setlocalversion  
+sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./msm-kernel/scripts/setlocalversion
+sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./external/dtc/scripts/setlocalversion
 
 cd ../../
-./kernel_platform/oplus/build/oplus_build_kernel.sh pineapple gki
+./oplus/build/oplus_build_kernel.sh pineapple gki
 
-git clone https://github.com/Kernel-SU/AnyKernel3 --depth=1
-rm -rf ./AnyKernel3/.git
-cp kernel_workspace/kernel_platform/out/msm-kernel-pineapple-gki/dist/Image ./AnyKernel3/
-cd AnyKernel3
-ZIP_NAME="Anykernel3-${CONFIG}-android14-11-o-v$(date +%Y%m%d).zip"
-zip -r "../$ZIP_NAME" ./*
+cd kernel_workspace/kernel_platform/out/msm-kernel-pineapple-gki/dist
+curl -LO https://github.com/ShirkNeko/SukiSU_KernelPatch_patch/releases/download/0.11-beta/patch_linux
+chmod +x patch_linux
+./patch_linux
+rm -f Image
+mv oImage Image
