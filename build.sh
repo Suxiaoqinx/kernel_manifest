50022a646b5af0da65a37aaf70726ff38fd321e9
苏晓晴 <37541680+Suxiaoqinx@users.noreply.github.com>
2025-05-14 03:56:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -82,11 +82,12 @@ sed -i 's/check_defconfig//' ./common/build.config.gki
 sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./common/scripts/setlocalversion  
 sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./msm-kernel/scripts/setlocalversion
 sed -i '$s|echo "\$res"|echo "\-oki-Coolapk@Suxiaoqing"|' ./external/dtc/scripts/setlocalversion
-./kernel_platform/build_with_bazel.py -t pineapple gki
 
-#cd ../kernel_workspace/out/msm-kernel-pineapple-gki/dist
-#curl -LO https://github.com/ShirkNeko/SukiSU_KernelPatch_patch/releases/download/0.11-beta/patch_linux
-#chmod +x patch_linux
-#./patch_linux
-#rm -f Image
-#mv oImage Image
+./build_with_bazel.py -t pineapple gki
+
+cd /kernel_workspace/kernel_platform/out/msm-kernel-pineapple-gki/host
+curl -LO https://github.com/ShirkNeko/SukiSU_KernelPatch_patch/releases/download/0.11-beta/patch_linux
+chmod +x patch_linux
+./patch_linux
+rm -f Image
+mv oImage Image
