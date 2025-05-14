6ae3d1739b6c4f17fa3c53d47a24e59e57622642
苏晓晴 <37541680+Suxiaoqinx@users.noreply.github.com>
2025-05-14 16:38:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -178,7 +178,7 @@ done
 # ===== 编译内核 =====
 echo ">>> 开始编译内核..."
 cd "$WORKDIR/kernel_platform"
-./kernel_platform/oplus/build/oplus_build_kernel.sh "$BAZEL_TARGET" gki
+./oplus/build/oplus_build_kernel.sh "$BAZEL_TARGET" gki
 
 # ===== 选择使用 patch_linux (KPM补丁)=====
 OUT_DIR="./kernel_platform/out/msm-kernel-${BAZEL_TARGET}-gki/dist"
